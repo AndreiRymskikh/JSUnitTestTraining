5ade89cc673013b090146284d27c8d95fa14c5db
ICX\Andrei.Rymskikh <andrei.rymskikh@itechart-group.com>
2024-08-16 08:24:51 +0200

    Test was refactored accourding with AAA pattern

--- code/basic-testing-frontend/src/math.test.js
+++ code/basic-testing-frontend/src/math.test.js
@@ -2,7 +2,16 @@ import { test, expect } from 'vitest';
 import {add} from './math';
 
 test('Summarize all number values in an array', () => {
-    const result = add([1, 2, 3]);
+    //Arrange
+    const numbers = [1, 2, 3];
+    const expectedResult = numbers.reduce(
+        (prevValue, currentValue) => prevValue + currentValue,
+        0
+    );
 
-    expect(result).toBe(6);
+    //Act
+    const result = add(numbers);
+
+    //Assert
+    expect(result).toBe(expectedResult);
 });
