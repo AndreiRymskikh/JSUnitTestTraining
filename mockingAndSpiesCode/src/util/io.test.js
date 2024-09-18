fe5f998733dfc617f2898a46abef9f1be7e284cb
Andrei.Rymskikh <v540509@mwiah.co.uk>
2024-09-18 08:47:42 +0200

    mock function in the test

--- mockingAndSpiesCode/src/util/io.test.js
+++ mockingAndSpiesCode/src/util/io.test.js
@@ -1,9 +1,14 @@
-import {it, expect} from 'vitest';
+import {it, expect, vi} from 'vitest';
 import writeData from './io';
+import { promises as fs } from 'fs';
+
+vi.mock('fs');
 
 it('should execute the writeFile method', () => {
     const testData = 'Test';
     const testFile = 'test.txt'
 
-    expect(writeData(testData, testFile)).resolves.toBeUndefined();
+    writeData(testData, testFile);
+
+    expect(fs.writeFile).toBeCalled();
 });
