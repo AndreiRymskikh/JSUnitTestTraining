d611fce28e46e23341cf0f9f26ea929ebf0c5ae2
Andrei.Rymskikh <v540509@mwiah.co.uk>
2024-10-01 08:22:11 +0200

    Virtual html page was prepared for the DOM test

---
+++ savePostForm/util/dom.test.js
@@ -0,0 +1,19 @@
+import {it, vi} from 'vitest';
+import {Window} from 'happy-dom';
+
+import fs from 'fs';
+import path from 'path';
+
+import { showError } from './dom';
+
+const htmlDocPath = path.join(process.cwd(), 'index.html');
+const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString();
+
+const window = new Window();
+const document = window.document;
+document.write(htmlDocumentContent);
+vi.stubGlobal('document', document);
+
+it('DOM test', () => {
+    showError('test');
+});
